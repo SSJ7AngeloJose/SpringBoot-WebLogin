f5de94ad916c8ffa922526626d847e677db3795f
SSJ7AngeloJose <angelo7ssj@gmail.com>
2024-06-12 00:07:52 -0500

    actualizado

--- src/main/java/SegurityConfig/done_AngeloJoseAgurto/service/UserService.java
+++ src/main/java/SegurityConfig/done_AngeloJoseAgurto/service/UserService.java
@@ -4,7 +4,7 @@ package SegurityConfig.done_AngeloJoseAgurto.service;
 import SegurityConfig.done_AngeloJoseAgurto.model.User;
 import SegurityConfig.done_AngeloJoseAgurto.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +12,8 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     public void save(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setEnabled(true);
         userRepository.save(user);
 
@@ -25,7 +22,7 @@ public class UserService {
     public void changePassword(String username, String newPassword) {
         User user = userRepository.findByUsername(username);
         if (user != null) {
-            user.setPassword(passwordEncoder.encode(newPassword));
+            user.setPassword(newPassword);
             userRepository.save(user);
         }
     }
